51c58ff6f52abbd038717dc51c0083a7b5e90726
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2022-01-22 12:39:43 +0530

    add: transfer

--- cmd/utils.go
+++ cmd/utils.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/common"
+	"github.com/portto/solana-go-sdk/program/sysprog"
 	"github.com/portto/solana-go-sdk/rpc"
 	"github.com/portto/solana-go-sdk/types"
 )
@@ -67,37 +69,41 @@ func RequestAirdrop(amount uint64) (string, error) {
 	return txhash, nil
 }
 
-// func Transfer(receiver string, amount uint64) (string, error){
-// 	// fetch the most recent blockhash 
-//     wallet, _ := ImportOldWallet(rpc.DevnetRPCEndpoint) 
-//     response, err := wallet.c.GetRecentBlockhash(context.TODO()) 
-//     if err != nil { 
-//         return "", err 
-//     } 
+func Transfer(receiver string, amount uint64) (string, error){
+	// fetch the most recent blockhash 
+    wallet, _ := ImportOldWallet(rpc.DevnetRPCEndpoint) 
+    response, err := wallet.c.GetRecentBlockhash(context.TODO()) 
+    if err != nil { 
+        return "", err 
+    } 
 
-//     // make a transfer message with the latest block hash 
-//     message := types.NewMessage( 
-//         wallet.account.PublicKey, // public key of the transaction signer
-//         []types.Instruction{
-//             sysprog.Transfer( 
-//                 wallet.account.PublicKey, // public key of the transaction sender 
-//                 common.PublicKeyFromString(receiver), // wallet address of the transaction receiver 
-//                 amount,                               // transaction amount in lamport 
-//             ), 
-//         }, 
-//         response.Blockhash, // recent block hash 
-//     ) 
+    // make a transfer message with the latest block hash 
+    message := types.NewMessage( 
+		types.NewMessageParam{
+			wallet.account.PublicKey, // public key of the transaction signer
+			[]types.Instruction{
+				sysprog.Transfer( 
+					sysprog.TransferParam{
+						wallet.account.PublicKey, // public key of the transaction sender 
+						common.PublicKeyFromString(receiver), // wallet address of the transaction receiver 
+						amount,
+					},                   // transaction amount in lamport 
+				),
+			}, 
+			response.Blockhash, // recent block hash
+		}
+) 
 
-//     // create a transaction with the message and TX signer 
-//     tx, err := types.NewTransaction(message, []types.Account{wallet.account, wallet.account}) 
-//     if err != nil { 
-//         return "", err 
-//     } 
+    // create a transaction with the message and TX signer 
+    tx, err := types.NewTransaction( types.NewTransactionParam{ message, []types.Account{wallet.account, wallet.account}}) 
+    if err != nil { 
+        return "", err 
+    } 
 
-//     // send the transaction to the blockchain 
-//     txhash, err := wallet.c.SendTransaction2(context.TODO(), tx) 
-//     if err != nil { 
-//         return "", err 
-//     } 
-//     return txhash, nil 
-// }
+    // send the transaction to the blockchain 
+    txhash, err := wallet.c.SendTransaction(context.TODO(), tx) 
+    if err != nil { 
+        return "", err 
+    } 
+    return txhash, nil 
+}
